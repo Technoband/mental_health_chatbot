b3e02f899d945cc60b3e1046674d94e3d3e969a8
Yashraj Zope <yashajzope143@gmail.com>
2024-04-15 20:17:42 +0530

    paths added

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-
+import requests
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 
 
 # Load the model, label encoder, and tokenizer
-# model = joblib.load('/content/drive/MyDrive/mha/model.h5')
-model = load_model('D:/mha/lstm_model.h5')
-label_encoder = joblib.load('D:/mha/label_encoder.pkl')
-tokenizer = joblib.load('D:/mha/tokenizer.pkl')
+# model = load_model('D:/mha/lstm_model.h5')
+# label_encoder = joblib.load('D:/mha/label_encoder.pkl')
+# tokenizer = joblib.load('D:/mha/tokenizer.pkl')
+# Define paths for model files on GitHub
+MODEL_URL = 'https://raw.githubusercontent.com/Technoband/mental_health_chatbot/main/models/lstm_model.h5'
+LABEL_ENCODER_URL = 'https://raw.githubusercontent.com/Technoband/mental_health_chatbot/main/models/label_encoder.pkl'
+TOKENIZER_URL = 'https://raw.githubusercontent.com/Technoband/mental_health_chatbot/main/models/tokenizer.pkl'
+
+# Load the model, label encoder, and tokenizer from GitHub
+model = load_model(requests.get(MODEL_URL, allow_redirects=True))
+label_encoder = joblib.load(requests.get(LABEL_ENCODER_URL, allow_redirects=True).content)
+tokenizer = joblib.load(requests.get(TOKENIZER_URL, allow_redirects=True).content)
 
 app = Flask(__name__)
 MAX_SEQUENCE_LENGTH = 100
